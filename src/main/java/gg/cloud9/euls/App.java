71d47d73429b895a32699bb8b46dd5735a266c92
Young Kim <shadowing71@gmail.com>
2014-04-06 01:56:56 -0700

    Update App to show an example of how Replay is used

--- src/main/java/gg/cloud9/euls/App.java
+++ src/main/java/gg/cloud9/euls/App.java
@@ -3,13 +3,27 @@ package gg.cloud9.euls;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class App {
-	Logger logger = LoggerFactory.getLogger(App.class);
-	public static void main(String[] args) {
-		new App().run(args);
-	}
+import java.io.IOException;
 
-	private void run(String[] args) {
-		logger.debug("Hello World!");
-	}
+public class App {
+    Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        new App().run(args);
+    }
+
+    private void run(String[] args) {
+        try {
+            Replay matchReplay = new Replay(args[0]);
+            GameInfo gameInfo = matchReplay.getGameInfo();
+
+            while (matchReplay.tick()) {
+                // This is where you want to do your main parsing or calculations
+                // The example shown here is to get information about the game
+                logger.debug(gameInfo.getCurrentGameState());
+            }
+        } catch (IOException e) {
+            logger.error(args[0] + " could not be opened");
+        }
+    }
 }
