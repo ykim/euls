55790347178cb9c77e6df0f5861538e64ac4558d
Young Kim <shadowing71@gmail.com>
2014-04-27 08:16:33 +0300

    Proper use of SL4J logging mechanism

--- src/main/java/gg/cloud9/euls/App.java
+++ src/main/java/gg/cloud9/euls/App.java
@@ -27,9 +27,9 @@ public class App {
 
                 // GameRules example
                 GameRules gameRules = matchReplay.getGameRules();
-                logger.info("Match Id", gameRules.getMatchId());
-                logger.info("Match Type", gameRules.getGameMode());
-                logger.info("Match Time", gameRules.getCurrentGameTime());
+                logger.info("Match Id : {}", gameRules.getMatchId());
+                logger.info("Match Type : {}", gameRules.getGameMode());
+                logger.info("Match Time : {}", gameRules.getCurrentGameTime());
 
                 // DotaPlayer example
                 logger.info("Player Iteration");
@@ -41,20 +41,20 @@ public class App {
                         DotaHero playerHero = player.getHero();
 
                         if (playerHero != null) {
-                            logger.info("Hero", playerHero.getHero());
-                            logger.info("Level", playerHero.getCurrentLevel());
-                            logger.info("HP", playerHero.getCurrentHP());
-                            logger.info("Mana", playerHero.getCurrentMana());
-                            logger.info("Coordinates", playerHero.getCoordinates());
-                            logger.info("Last Hits", player.getCurrentLastHits());
-                            logger.info("Denies", player.getCurrentDenies());
+                            logger.info("Hero : {}", playerHero.getHero());
+                            logger.info("Level : {}", playerHero.getCurrentLevel());
+                            logger.info("HP : {}", playerHero.getCurrentHP());
+                            logger.info("Mana : {}", playerHero.getCurrentMana());
+                            logger.info("Coordinates : {}", playerHero.getCoordinates());
+                            logger.info("Last Hits : {}", player.getCurrentLastHits());
+                            logger.info("Denies : {}", player.getCurrentDenies());
 
                             // DotaItem eample
                             List<DotaItem> items = player.getItems();
                             for (int j = 0; j < items.size(); j++) {
                                 DotaItem item = items.get(j);
                                 if (item != null) {
-                                    logger.info("Item " + j, item.getItem());
+                                    logger.info("Item : {}" + j, item.getItem());
                                 }
                             }
                         }
@@ -66,7 +66,7 @@ public class App {
                 List<DotaCourier> couriers = matchReplay.getCouriersByTeam(Team.RADIANT);
                 for (DotaCourier courier : couriers) {
                     if (courier != null) {
-                        logger.info("Coordinate", courier.getCoordinates());
+                        logger.info("Coordinate : {}", courier.getCoordinates());
                     }
                 }
 
@@ -75,9 +75,9 @@ public class App {
                 List<DotaTower> towers = matchReplay.getTowersByTeam(Team.DIRE);
                 for (DotaTower tower : towers) {
                     if (tower != null) {
-                        logger.info("Tier", tower.getTier());
-                        logger.info("Lane", tower.getLane());
-                        logger.info("HP", tower.getCurrentHP());
+                        logger.info("Tier : {}", tower.getTier());
+                        logger.info("Lane : {}", tower.getLane());
+                        logger.info("HP : {}", tower.getCurrentHP());
                     }
                 }
 
@@ -87,9 +87,9 @@ public class App {
                 List<DotaLaneCreep> creeps = matchReplay.getLaneCreepsByTeam(Team.RADIANT);
                 for (DotaLaneCreep creep : creeps) {
                     if (creep != null) {
-                        logger.info("Name", creep.getName());
-                        logger.info("Coordinate", creep.getCoordinates());
-                        logger.info("HP", creep.getCurrentHP());
+                        logger.info("Name : {}", creep.getName());
+                        logger.info("Coordinate : {}", creep.getCoordinates());
+                        logger.info("HP : {}", creep.getCurrentHP());
                     }
                 }
 
@@ -97,16 +97,16 @@ public class App {
                 logger.info("Neutral Creeps");
                 List<DotaNeutralCreep> neutrals = matchReplay.getNeutralCreeps();
                 for (DotaNeutralCreep neutralCreep : neutrals) {
-                    logger.info("Name", neutralCreep.getName());
-                    logger.info("Coordinate", neutralCreep.getCoordinates());
-                    logger.info("HP", neutralCreep.getCurrentHP());
+                    logger.info("Name : {}", neutralCreep.getName());
+                    logger.info("Coordinate : {}", neutralCreep.getCoordinates());
+                    logger.info("HP : {}", neutralCreep.getCurrentHP());
                 }
 
                 // DotaRoshan example
                 logger.info("Roshan");
                 DotaRoshan roshan = matchReplay.getRoshan();
                 if (roshan != null) {
-                    logger.info("HP", roshan.getCurrentHP());
+                    logger.info("HP : {}", roshan.getCurrentHP());
                 }
 
                 // Ward Examples
@@ -114,7 +114,7 @@ public class App {
                 List<DotaWard> wards = matchReplay.getObserverWardsByTeam(Team.DIRE);
                 for (DotaWard ward : wards) {
                     if (ward != null) {
-                        logger.info("Coordinate", ward.getCoordinates());
+                        logger.info("Coordinate : {}", ward.getCoordinates());
                     }
                 }
             }
